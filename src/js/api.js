e84027362e299e608da407d66b36d1dc36bc0dc0
Vlad Kovalynskyi <99043529+vladkovalynskyi@users.noreply.github.com>
2023-06-17 18:55:42 +0300

    add style, re api.js, try index

--- src/js/api.js
+++ src/js/api.js
@@ -6,11 +6,12 @@ const API_KEY = '37352949-d51cc3913c6f19a152f1908cc';
 axios.defaults.baseURL = 'https://pixabay.com/api/'
 
 export async function getData(searchQuery) {
-    const res = await axios.get(getRequestUrl(searchQuery));
-    if (res.status !== 200) {
-        throw new Notiflix.Notify.failure(response.statusText || response.status);
+    try {
+        const res = await axios.get(getRequestUrl(searchQuery));
+        return res.data;
+    } catch(error) {
+        throw Error(error.response?.statusText || error.message);
     }
-    return res.data;
 }
 
 export function getRequestUrl(searchQuery) {
--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,130 @@
-import axios from 'axios';
+import { getData } from "./api";
 import Notiflix from 'notiflix';
-import simpleLightbox from 'simplelightbox';
-import { getData } from './api';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const formEl = document.getElementById('search-form');
 const galleryEl = document.querySelector('.gallery');
 const loadBtn = document.querySelector('.load-more');
 export let currentPage = 1;
 export const perPage = 40;
+let currentSearchQuery = '';
+let totalHits = 0;
 
-formEl.addEventListener('submit', function (e) {
-    e.preventDefault();
-    clearGallery();
-    performSearch();
+const galleryN = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 200,
+  captionsData: 'alt'
 });
 
-loadBtn.addEventListener('click', performSearch());
+formEl.addEventListener('submit', onFormSubmit);
+loadBtn.addEventListener('click', loadMore);
 
-let searchQuery = '';
+async function loadHeadlines(searchQuery) {
+  currentSearchQuery = searchQuery;
 
+  const data = await getData(searchQuery);
+  if (data.hits && data.hits.length > 0) {
+    totalHits = data.totalHits;
+    toggleLoadMoreButton();
+    return data.hits;
+  }
+}
 
+function renderHeadLine({ largeImageURL, tags, likes, views, comments, downloads }) {
+  return `<div class="photo-card">
+    <a class="gallery__link" href="${largeImageURL}">
+      <img src="${largeImageURL}" alt="${tags}" loading="lazy" />
+      <div class="info">
+        <p class="info-item">
+          <b>Likes: ${likes}</b>
+        </p>
+        <p class="info-item">
+          <b>Views: ${views}</b>
+        </p>
+        <p class="info-item">
+          <b>Comments: ${comments}</b>
+        </p>
+        <p class="info-item">
+          <b>Downloads: ${downloads}</b>
+        </p>
+      </div>
+    </a>
+  </div>`;
+}
+
+function renderHeadlinesList(headlines) {
+  if (currentPage === 1) {
+    galleryEl.innerHTML = '';
+  }
+
+  const renderedHTML = headlines
+    .map(headline => renderHeadLine(headline))
+    .join("");
+
+  galleryEl.insertAdjacentHTML("beforeend", renderedHTML);
+  refreshSimpleLightbox();
+
+  const { height: cardHeight } = galleryEl.firstElementChild.getBoundingClientRect();
+  window.scrollBy({
+    top: cardHeight * 0.75,
+    behavior: "smooth",
+  });
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const searchInput = this.searchQuery;
+  const searchQuery = searchInput.value.trim();
+
+  if (searchQuery === '') {
+    return;
+  }
+
+  currentPage = 1;
+
+  loadHeadlines(searchQuery)
+    .then(data => {
+      renderHeadlinesList(data);
+      checkEndOfResults();
+    })
+    .catch(error => {
+      console.error(error);
+      Notiflix.Notify.failure('<p>Sorry, there are no images matching your search query. Please try again.</p>');
+      hideLoadMoreButton();
+    });
+}
+
+function loadMore() {
+  currentPage++;
+
+  loadHeadlines(currentSearchQuery)
+    .then(headlines => {
+      renderHeadlinesList(headlines);
+      checkEndOfResults();
+    });
+}
+
+function hideLoadMoreButton() {
+  loadBtn.style.display = 'none';
+}
+
+function showLoadMoreButton() {
+  loadBtn.style.display = 'block';
+}
+
+function checkEndOfResults() {
+  if (totalHits <= currentPage * perPage) {
+    hideLoadMoreButton();
+    Notiflix.Notify.failure('We\'re sorry, but you\'ve reached the end of search results.');
+  }
+}
+
+function toggleLoadMoreButton() {
+  totalHits <= currentPage * perPage ? hideLoadMoreButton() : showLoadMoreButton();
+}
+
+function refreshSimpleLightbox() {
+  galleryN.refresh();
+}
